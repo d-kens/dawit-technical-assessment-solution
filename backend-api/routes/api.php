8d41c9a5062455ce4355f1a11b37a96c6dfe12a7
d-kens <omondikens1@gmail.com>
2024-02-26 11:58:56 +0300

    API route authentication

--- backend-api/routes/api.php
+++ backend-api/routes/api.php
@@ -23,6 +23,9 @@ Route::prefix('v1')->group(function () {
 
 });
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('register', [\App\Http\Controllers\AuthController::class, 'register']);
+Route::post('login', [\App\Http\Controllers\AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('logout', [\App\Http\Controllers\AuthController::class, 'logout']);
 });
--- backend-api/routes/api/v1/clients.php
+++ backend-api/routes/api/v1/clients.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::middleware([
-//    'auth:api',
+   'auth:sanctum',
 ])
     ->name('clients.')
     ->namespace("\App\Http\Controllers")
